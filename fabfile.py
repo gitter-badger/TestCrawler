659a7586f9beeefe2819b8ccd2e493f19da001ff
Valentin <valentin.menardie@gmail.com>
2015-10-09 09:48:10 +0200

    Added composer commandwq

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,11 @@
 from fabric.api import *
 from fabric_aws import *
 
-
 env.user='ubuntu'
-env.key_filename = ['/home/nek/key-maxime.pem'];
+env.key_filename = ['key.pem'];
 
 @autoscaling_group('us-east-1', 'maxime-asg')
 @task
 def deploy():
     sudo("cd /var/www/html/TestCrawler && git pull")
-
+    sudo("composer install")
